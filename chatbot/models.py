792678aa4f8bd4535a513ab188ef66b2803f2025
Manish Patil <273manishp@gmail.com>
2024-01-20 20:53:18 +0530

    new

---
+++ chatbot/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    response = models.TextField()
+    like = models.BooleanField(default=False)
+    unlike = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.message}'
